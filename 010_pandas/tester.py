00e7f85975f4b7970d3ba9c71e4d878ef3555637
Roman <roman@gamma-intelligence.com>
2021-10-01 19:12:05 +0300

    Pandas tester

---
+++ 010_pandas/tester.py
@@ -0,0 +1,5 @@
+people = {
+    "first": ['Bob', 'Alex', 'John', 'Mary'],
+    "last": ['Smith', 'Green', 'Black', 'Gold'],
+    "email": ['bob@example.com', 'alex@example.com', "john@example.com", 'mary@example.com']
+}
